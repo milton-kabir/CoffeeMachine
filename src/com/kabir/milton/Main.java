e4f8b6e2fadd3dc205fff3a37a023797e808943e
Milton Kabir <drifter.kabir@gmail.com>
2021-07-06 20:32:47 +0600

    Second Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,15 +1,17 @@
 package com.kabir.milton;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need: ");
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        System.out.println("For "+c+" cups of coffee you will need:\n" +
+                c*200+" ml of water\n" +
+                c*50+" ml of milk\n" +
+                c*15+" g of coffee beans");
     }
 }
