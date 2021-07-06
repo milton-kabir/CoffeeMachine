b7fa8be62ccb4ef78b9a4f77e62dab406646b18a
Milton Kabir <drifter.kabir@gmail.com>
2021-07-06 21:10:49 +0600

    Fourth Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -6,28 +6,57 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int cnt=1123456789;
         Scanner sc= new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int w=sc.nextInt();
-        cnt=Math.min(cnt,(w/200));
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        int m=sc.nextInt();
-        cnt=Math.min(cnt,m/50);
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        int g=sc.nextInt();
-        cnt=Math.min(cnt,g/15);
-        System.out.println("Write how many cups of coffee you will need: ");
-        int c=sc.nextInt();
-        if(cnt<c){
-            System.out.println("No, I can make only "+cnt+" cup(s) of coffee");
+        int w=400,m=540, g=120, cp=9,dl=550;
+        System.out.println("The coffee machine has:\n" +
+                w+" ml of water\n" +
+                m+" ml of milk\n" +
+                g+" g of coffee beans\n" +
+                cp+" disposable cups\n" +
+                "$"+dl+" of money");
+        System.out.println("Write action (buy, fill, take): ");
+        String st=sc.nextLine();
+        if(st.equals("buy")){
+            System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+            int cnt=sc.nextInt();
+            if(cnt==1){
+                w-=250;
+                g-=16;
+                dl+=4;
+            }
+            else if(cnt==2){
+                w-=350;
+                m-=75;
+                g-=20;
+                dl+=7;
+            }
+            else{
+                w-=200;
+                m-=100;
+                g-=12;
+                dl+=6;
+            }
+            cp--;
         }
-        else if(cnt==c){
-            System.out.println("Yes, I can make that amount of coffee");
+        else if(st.equals("fill")){
+            System.out.println("Write how many ml of water you want to add:");
+            w+=sc.nextInt();
+            System.out.println("Write how many ml of milk you want to add: ");
+            m+=sc.nextInt();
+            System.out.println("Write how many grams of coffee beans you want to add: ");
+            g+=sc.nextInt();
+            System.out.println("Write how many disposable cups of coffee you want to add: ");
+            cp+=sc.nextInt();
         }
         else{
-            System.out.println("Yes, I can make that amount of coffee (and even "+(cnt-c)+" more than that)");
+            System.out.println("I gave you $550");
+            dl=0;
         }
-
+        System.out.println("The coffee machine has:\n" +
+                w+" ml of water\n" +
+                m+" ml of milk\n" +
+                g+" g of coffee beans\n" +
+                cp+" disposable cups\n" +
+                "$"+dl+" of money");
     }
 }
